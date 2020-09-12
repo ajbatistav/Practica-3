876be2e6b45b54c0b874859590c9c98d985eabbc
Alejandro Batista <ajbatistav@gmail.com>
2020-09-12 18:08:14 -0500

    casi terminado

--- ex3.py
+++ ex3.py
@@ -1,10 +1,10 @@
-def bisiesto():
- year = int(input("ingrese un año: "))
- test = year % 4
- if test == 0:
+def bisiesto(year):
+  test = year % 4
+  if test == 0:
     print(f"{year} es bisiesto")
- else:
+  else:
     print(f"{year} no es bisiesto")
 
 
-bisiesto()
+y= int(input("ingrese un año: "))
+bisiesto(y)
---
+++ ex4.py
@@ -0,0 +1,11 @@
+
+n_1 = float(input("Ingrese el primer numero: "))
+n_2 = float(input("Ingrese el segundo numero: "))
+
+
+def igualdad():
+    return n_1 == n_2
+     
+        
+res = igualdad()
+print(res)
