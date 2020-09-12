18b8c7b7ae12556d55f25ca9e8be5a6c4997af71
Alejandro Batista <ajbatistav@gmail.com>
2020-09-12 17:45:52 -0500

    ex3.py completo

---
+++ ex2.py
@@ -0,0 +1,29 @@
+def escrito():
+    
+    if numero == 1:
+        print("Uno")
+    elif numero == 2:
+        print("Dos")
+    elif numero == 3:
+        print("Tres")
+    elif numero == 4:
+        print("Cuatro")
+    elif numero == 5:
+        print("Cinco")
+    elif numero == 6:
+        print("Seis")
+    elif numero == 7:
+        print("Siete")
+    elif numero == 8:
+        print("Ocho")
+    elif numero == 9:
+        print("Nueve")
+    elif numero == 10:
+        print("Diez")
+    else:
+        print("Su numero no esta dentro del rango")
+    
+
+
+numero = int(input("Por favor ingrese un numero del 1 al 10: > "))
+escrito()
